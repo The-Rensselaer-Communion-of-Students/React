77a643b95c3c0506c40d7601430888e3d89a43f9
Maxwell Montemagno <55475323+maxmonte15@users.noreply.github.com>
2022-04-25 22:24:01 -0400

    Added link between Registerpage.jsx and Loginpage.jsx, cleaned up code

--- cloud_drive/src/Pages/Loginpage.jsx
+++ cloud_drive/src/Pages/Loginpage.jsx
@@ -6,7 +6,6 @@ import { useNavigate } from "react-router-dom";
 import "./Loginpage.css";
 import Navbarf from '../Components/Navbarf';
 
-
 export default function Loginpage() {
         const emailRef = useRef();
         const psdRef = useRef();
@@ -41,34 +40,12 @@ export default function Loginpage() {
                 <Button variant="primary" type="submit" size="lg" className='rounded-pill border-0 shadow'>Sign In With Google</Button>
                 <Button variant="primary" type="button" size="lg" className='rounded-pill border-0 shadow' onClick={resetRedirect}>Reset Password</Button>
               </div>
+              <div id="dontHave">
+                <a href="register">Don't have an account?</a>
+              </div>
           </Form>
         </div>
     </div>
     </>
-/*
-      <>
-      <Navbarf/>
-      <div id="w">
-          <div id="w2">
-              <h1>Sign In to Your Account</h1>
-              {error && <p className="error">{error}</p>}
-              <Form id="login" onSubmit={onSubmit}>
-                  <Form.Label>Email Adress</Form.Label>
-                  <Form.Control type="email" placeholder="example@gmail.com" ref={emailRef} />
-                  <Form.Label>Password</Form.Label>
-                  <Form.Control type="password" placeholder="Password" ref={psdRef} />
-                  <hr className="solid" />
-                  <div className="d-grid gap-2">
-                      <Button variant="success" type="submit" size="lg">Sign In</Button>
-                      <p>or</p>
-                      <Button variant="primary" type="submit" size="lg">Sign In With Google</Button>
-                  </div>
-                  <div id="dontHave">
-                      {/* Forgot your password? stuff here [check userContext.js]}
-                      /*<a href="register">Don't have an account?</a>
-                  </div>
-              </Form>
-          </div>
-      </div></>*/
   )
 }
